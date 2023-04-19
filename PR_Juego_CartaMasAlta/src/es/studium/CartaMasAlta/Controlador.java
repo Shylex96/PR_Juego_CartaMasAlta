eb77d31550a0dfaf4d1fc9d53e8401b54ee13ff8
Shylex96 <77785532+Shylex96@users.noreply.github.com>
2023-04-19 09:59:55 +0200

    Add files via upload

--- PR_Juego_CartaMasAlta/src/es/studium/CartaMasAlta/Controlador.java
+++ PR_Juego_CartaMasAlta/src/es/studium/CartaMasAlta/Controlador.java
@@ -1,6 +1,9 @@
 package es.studium.CartaMasAlta;
 
-public class Controlador {
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+public class Controlador implements WindowListener {
 
 	Modelo modelo;
 	MenuPrincipal menuPrincipal;
@@ -8,5 +11,43 @@ public class Controlador {
 		
 		this.modelo = m;
 		this.menuPrincipal = mp;
+		
+		// Añadir Listener
+		this.menuPrincipal.addWindowListener(this);
+	}
+	@Override
+	public void windowOpened(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void windowClosing(WindowEvent e) {
+		System.exit(0);
+		
+	}
+	@Override
+	public void windowClosed(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void windowIconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void windowDeiconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void windowActivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void windowDeactivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
